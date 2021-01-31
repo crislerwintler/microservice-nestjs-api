8e3270c8cabfbf9ae88d724da2701dcbed486906
crislerwintler <crislerwintler@gmail.com>
2021-01-31 18:07:12 -0300

    add migrations

---
+++ src/migrations/1612127114681-ProductsNameTable.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class ProductsNameTable1612127114681 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
